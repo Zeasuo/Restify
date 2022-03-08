5b3d4b142814a8bdb8f6f0089718587496779012
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-08 14:08:56 -0500

    s

--- P2/restaurants/views/createRestaurant.py
+++ P2/restaurants/views/createRestaurant.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from restaurants.models import Restaurant
 from restaurants.serializers.createRestaurant import CreateRestaurantSerializer
 
 
 class CreateRestaurant(CreateAPIView):
     serializer_class = CreateRestaurantSerializer
     permission_class = [IsAuthenticated]
+    queryset = Restaurant.objects.all()
