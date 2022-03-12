8066253641c1b718a0e28f008fcda4e27587e51f
William Gao <gaoyi.william@gmail.com>
2022-03-12 18:58:42 -0500

    like in user model

--- P2/accounts/models.py
+++ P2/accounts/models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatar', null=True, blank=True)
     followers = models.IntegerField(default=0)
     followings = models.IntegerField(default=0)
-    like = models.IntegerField(default=0)
+    likes = models.ManyToManyField("restaurants.Restaurant", related_name="likes")
     birthday = models.DateTimeField(null=True, blank=True)
     phone_number = models.CharField(max_length=11, null=True, blank=True)
