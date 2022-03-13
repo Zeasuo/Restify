f443ba83ec99fe0f8f3af0d1f3db4b20f8d8b912
William Gao <gaoyi.william@gmail.com>
2022-03-12 22:51:24 -0500

    follow/unfollow

--- P2/accounts/models.py
+++ P2/accounts/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatar', null=True, blank=True)
-    followers = models.IntegerField(default=0)
-    followings = models.IntegerField(default=0)
-    likes = models.ManyToManyField("restaurants.Restaurant", related_name="likes")
     birthday = models.DateTimeField(null=True, blank=True)
     phone_number = models.CharField(max_length=11, null=True, blank=True)
+    followers = models.IntegerField(default=0)
+    followings = models.ManyToManyField("restaurants.Restaurant",
+                                        related_name="followings", blank=True)
+    likes = models.ManyToManyField("restaurants.Blog",
+                                   related_name="liked_blogs", blank=True)
--- P2/accounts/urls.py
+++ P2/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from accounts.views import socialNetwork
 from accounts.views.createUserView import CreateUserView
 from accounts.views.getUserView import GetUserView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -7,9 +8,12 @@ from accounts.views.updateUserView import UpdateUserView
 
 app_name = 'accounts'
 urlpatterns = [
-    path('user/<str:username>/details/', GetUserView.as_view(), name = 'user'),
+    path('user/<str:username>/details/', GetUserView.as_view(), name='user'),
     path('user/register/', CreateUserView.as_view(), name='create_user'),
-    path('user/update/', UpdateUserView.as_view(), name = 'update_user'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair')
-
+    path('user/update/', UpdateUserView.as_view(), name='update_user'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('follow/<int:restaurant_pk>', socialNetwork.follow_view,
+         name='follow'),
+    path('unfollow/<int:restaurant_pk>', socialNetwork.unfollow_view,
+         name='unfollow')
 ]
