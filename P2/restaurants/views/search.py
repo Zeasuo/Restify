2e934723e998a0e179815e57fcc389086b9f4a7d
Connor315 <gao315320130@gmail.com>
2022-03-14 12:06:58 -0400

    search

--- P2/restaurants/views/search.py
+++ P2/restaurants/views/search.py
@@ -11,10 +11,8 @@ class SearchView(ListAPIView):
     # https://simpleisbetterthancomplex.com/tips/2016/06/20/django-tip-5-how-to-merge-querysets.html
     queryset = Restaurant.objects.all()
     serializer_class = SearchSerializer
-    # filter_backends = [filters.OrderingFilter]
-    # ordering_fields = ['restaurant_name', 'address', 'description']
-    # ordering = ['followers_num']
-    # ordering = ['id']
+    filter_backends = (filters.OrderingFilter, filters.SearchFilter,)
     search_fields = ['restaurant_name', 'address']
-    filter_backends = (filters.SearchFilter,)
+    ordering = ['-follower_num']
+
 
