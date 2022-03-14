99d506fcf4bc599f0f7f6030dfacec81f85302d2
William Gao <gaoyi.william@gmail.com>
2022-03-14 19:13:08 -0400

    follow

--- P2/socials/views/followRestaurant.py
+++ P2/socials/views/followRestaurant.py
@@ -1,21 +1,14 @@
-from rest_framework.exceptions import APIException
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 
 from socials.models import Follow
 from socials.serializers import followRestaurantSerializer
 
 
-class FollowRestaurantView(APIView):
+class FollowRestaurantView(CreateAPIView):
     serializer_class = followRestaurantSerializer
     permission_class = [IsAuthenticated]
     queryset = Follow.objects.all()
 
-    def post(self, request, *args, **kwargs):
-        try:
-            Follow.objects.get(user=self.request.user, restaurant=self.request.restaurant_name)
-            raise APIException("You already followed this restaurant")
-        except Follow.DoesNotExist:
-            return super().post(request, *args, **kwargs)
 
 
