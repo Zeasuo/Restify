b8b12ca13ce326421241d0efc465d9ae650633cf
Connor315 <gao315320130@gmail.com>
2022-03-14 16:25:46 -0400

    might be right

--- P2/restaurants/views/searchRestaurant.py
+++ P2/restaurants/views/searchRestaurant.py
@@ -1,5 +1,6 @@
 from itertools import chain
 
+from django.db.models import Count
 from rest_framework import filters
 from rest_framework.generics import ListAPIView
 
@@ -14,8 +15,9 @@ class SearchRestaurantView(ListAPIView):
     # https://www.codeproject.com/Questions/5308354/Django-searchfilter-for-multiple-models
     # https://medium.com/swlh/searching-in-django-rest-framework-45aad62e7782
     # https://stackoverflow.com/questions/45589644/django-order-by-desc
-    queryset = Restaurant.objects.all()
+    queryset = Restaurant.objects.all().\
+        annotate(follower_num=Count('followers')).order_by('-follower_num')
     serializer_class = SearchRestaurantSerializer
     filter_backends = (filters.OrderingFilter, filters.SearchFilter,)
     search_fields = ['restaurant_name', 'address']
-    ordering = ['-follower_num']
+    # ordering = ['-follower_num']
