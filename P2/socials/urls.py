900cf8d70d05ed777f270cdb92b58b957c9bc270
William Gao <gaoyi.william@gmail.com>
2022-03-15 12:04:47 -0400

    follow

--- P2/socials/urls.py
+++ P2/socials/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from socials.views.addBlog import AddBlog
-from socials.views.followRestaurant import FollowRestaurantView
+from socials.views.followRestaurant import FollowRestaurantView, \
+    UnFollowRestaurantView
 from socials.views.getBlog import GetBlogView
 
 app_name = 'socials'
 
 urlpatterns = [
     path('follow/<str:restaurant_name>/', FollowRestaurantView.as_view(), name='create_restaurant'),
+    path('unfollow/<str:restaurant_name>/', UnFollowRestaurantView.as_view(), name='unfollow_restaurant'),
     path('get_blog/<str:restaurant_name>/', GetBlogView.as_view(), name='get_blog'),
     path('add_blog/', AddBlog.as_view(), name='add_blog'),
 ]
--- P2/socials/views/followRestaurant.py
+++ P2/socials/views/followRestaurant.py
@@ -1,7 +1,10 @@
 from django.core.exceptions import BadRequest
+from rest_framework import status
 from rest_framework.exceptions import APIException
-from rest_framework.generics import CreateAPIView, get_object_or_404
+from rest_framework.generics import CreateAPIView, DestroyAPIView, \
+    get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from restaurants.models import Restaurant
 from socials.models import Follow
@@ -25,3 +28,21 @@ class FollowRestaurantView(CreateAPIView):
         request.data.update({'user': request.user.pk, 'restaurant': get_object_or_404(Restaurant, restaurant_name=kwargs[
             'restaurant_name']).pk})
         return super(FollowRestaurantView, self).create(request, *args, **kwargs)
+
+
+class UnFollowRestaurantView(DestroyAPIView):
+    serializer_class = FollowRestaurantSerializer
+    permission_class = [IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        curr_restaurant = get_object_or_404(Restaurant, restaurant_name=kwargs[
+            'restaurant_name'])
+
+        try:
+            follow = Follow.objects.get(user=self.request.user,restaurant=curr_restaurant)
+        except:
+            raise BadRequest("You have not followed this restaurant")
+
+        follow.delete()
+
+        return Response(status.HTTP_204_NO_CONTENT)
