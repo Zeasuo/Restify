26b0f3f5207b160b7253c3b8f22dc3c2eabca4ef
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-15 16:34:53 -0400

    s

--- P2/accounts/views/loginView.py
+++ P2/accounts/views/loginView.py
@@ -14,14 +14,18 @@ class LoginView(GenericAPIView):
     permission_classes = (AllowAny,)
 
     def post(self, request):
+        if 'username' not in request.data:
+            return Response('Username is Required', status.HTTP_400_BAD_REQUEST)
+        if 'password' not in request.data:
+            return Response('Password is Required', status.HTTP_400_BAD_REQUEST)
         username = request.data['username']
         password = request.data['password']
         user = authenticate(username=username, password=password)
+        if not user:
+            message = "Wrong username or password"
+            return Response(message)
         token = Token.objects.get_or_create(user=user)
         if token:
             login(request, user)
             json = {"token": token[0].key}
             return Response(json, status=status.HTTP_200_OK)
-        else:
-            message = "Wrong username or password"
-            return Response(message)
