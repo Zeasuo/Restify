9f65427a4b69fd651844568a0746cd895e22e650
Connor315 <gao315320130@gmail.com>
2022-03-15 18:51:47 -0400

    fix bug

--- P2/socials/views/addNotification.py
+++ P2/socials/views/addNotification.py
@@ -3,6 +3,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from accounts.models import User
+from restaurants.models import Restaurant
+from socials.models import Blog
 from socials.serializers.addNotification import AddNotificationSerializer
 
 
@@ -15,5 +17,8 @@ class AddNotificationView(CreateAPIView):
             raise BadRequest('The target user is not exist')
         elif (request.data['action'] == 'comment' or request.data['action'] == 'follow') and request.data['Target'] == 'blog':
             raise BadRequest('Your action is not appropriate for a blog')
-
+        elif ('blog_id' not in request.data or not Blog.objects.filter(id=request.data['blog_id']).exists()) and request.data['Target'] == 'blog':
+            raise BadRequest('You need to have a correct object to like/follow/comment')
+        elif ('restaurant_id' not in request.data or not Restaurant.objects.filter(id=request.data['restaurant_id']).exists()) and request.data['Target'] == 'rest':
+            raise BadRequest('You need to have a correct object to like/follow/comment')
         return self.create(request, *args, **kwargs)
