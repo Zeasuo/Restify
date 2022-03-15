c311064888cc7ef97998c6cb649aa3e008a2aec0
Connor315 <gao315320130@gmail.com>
2022-03-15 11:26:11 -0400

    update search, addMenu and implement addBlog, getBlog

--- P2/socials/serializers/addBlog.py
+++ P2/socials/serializers/addBlog.py
@@ -4,16 +4,12 @@ from restaurants.models import Restaurant
 from socials.models import Blog
 
 
-class AddPostSerializer(serializers.ModelSerializer):
-    restaurant = serializers.CharField(source='restaurant.get_name', read_only=True)
-    likes = serializers.IntegerField(default=0, read_only=True)
-    comment_num = serializers.IntegerField(default=0, read_only=True)
+class AddBlogSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Blog
-        fields = ['title', 'content', 'likes', 'comment_num', 'restaurant']
+        fields = ['title', 'content']
 
     def create(self, validated_data):
-        x = Restaurant.objects.get(owner=self.context['request'].user)
+        x = Restaurant.objects.get(restaurant_name=self.context['request'].user.restaurant)
         return super().create({**validated_data, **{'restaurant': x}})
-
--- P2/socials/views/addBlog.py
+++ P2/socials/views/addBlog.py
@@ -1,1 +1,15 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from socials.models import Blog
+from socials.serializers.addBlog import AddBlogSerializer
 
+
+class AddBlog(CreateAPIView):
+    serializer_class = AddBlogSerializer
+    permission_class = [IsAuthenticated]
+    queryset = Blog.objects.all()
+
+    def post(self, request, *args, **kwargs):
+
+        # https://stackoverflow.com/questions/3090302/how-do-i-get-the-object-if-it-exists-or-none-if-it-does-not-exist-in-django
+        return super().post(request, *args, **kwargs)
