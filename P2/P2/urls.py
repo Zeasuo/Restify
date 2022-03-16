0feec4fffe55887360afead616796bd2cacaf6f4
William Gao <gaoyi.william@gmail.com>
2022-03-15 23:14:38 -0400

    s

--- P2/P2/urls.py
+++ P2/P2/urls.py
@@ -23,9 +23,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="P2 API",
       default_version='v1',
-      description="Test description",
+      description="API Documentation for Group 0074 P2. ",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
