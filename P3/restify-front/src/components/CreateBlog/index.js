25a65f959fd1acca29d6fe2b14a5492a9fb9798b
Connor315 <gao315320130@gmail.com>
2022-04-19 16:02:06 -0400

    createblog

--- P3/restify-front/src/components/CreateBlog/index.js
+++ P3/restify-front/src/components/CreateBlog/index.js
@@ -1,19 +1,27 @@
 import { Container } from "react-bootstrap";
 import React, {useEffect, useState} from 'react';
+import { Form, Button} from 'react-bootstrap';
 
 // https://react-bootstrap.github.io/forms/overview/
+// https://stackoverflow.com/questions/51913522/reactjs-multiple-lines-of-input-using-form
 
 const CreateBlog = () =>{
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [check, setCheck] = useState(false)
+
     return <>
-        <Form>
+        <Form style={{marginLeft: "30%", marginTop: "10%"}}>
             <Form.Group className="mb-3">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter your blog tile here." />
+                <Form.Control type="text"
+                              style={{width:"50%"}}
+                              placeholder="Title" />
             </Form.Group>
 
             <Form.Group className="mb-3">
-                <Form.Label>Content</Form.Label>
-                <Form.Control type="text" placeholder="Enter your blog content here." />
+                <Form.Control as="textarea" rows="5"
+                              style={{width:"50%"}}
+                              placeholder="Text" />
             </Form.Group>
 
             <Form.Group className="mb-3">
