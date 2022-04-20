f382259fee110cfffbacf0699ef2d0ffe1aa1d12
William Gao <gaoyi.william@gmail.com>
2022-04-19 20:34:28 -0400

    idk

--- P3/restify-front/src/components/AddRestaurant/Details.js
+++ P3/restify-front/src/components/AddRestaurant/Details.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { useState } from "react";
+
 import { Container, Grid, TextField, Button, Paper } from "@material-ui/core";
 // import Image from '../images/details.jpg';
 
 const Details = ({ nextStep, handleChange, state }) => {
+    const [nameState, setName] = useState("");
+    const [addressState, setAddress] = useState("");
+    const [postalCodeState, setPostalCode] = useState("");
+    const [nameNotification, setNameNotification] = useState(true);
+    const [addressNotification, setAddressNotification] = useState(true);
+    const [postalCodeNotification, setPostalCodeNotification] = useState(true);
     // for continue event listener
     const Continue = (e) => {
         e.preventDefault();
-        nextStep();
+        if (nameState && addressState && postalCodeState) {
+            var {restaurant_name, address, postal_code} =
+                document.forms[1];
+            const formData = new FormData();
+            formData.append("restaurant_name", nameState);
+            formData.append("address", addressState);
+            formData.append("postal_code", postalCodeState);
+            fetch("http://127.0.0.1:8000/restaurants/create/", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Token " + localStorage.getItem("restifyToken"),
+                },
+                body: formData,
+            }).then((response) => {
+                if (!response.ok) {
+                    console.log(response)
+                }
+                else {
+                    nextStep();
+                }
+            });
+            
+        } else {
+            if (!nameState) {
+                setNameNotification(false);
+            }
+            if (!addressState) {
+                setAddressNotification(false);
+            }
+            if (!postalCodeState) {
+                setPostalCodeNotification(false);
+            }
+        }
     };
 
     //   const styles = {
@@ -14,30 +54,49 @@ const Details = ({ nextStep, handleChange, state }) => {
     //         backgroundImage: `url(${Image})`
     //         }
     //     };
-    
+
     const validate_name = (restaurantName) => {
-        // check if name already exists from database 
-        fetch("http://127.0.0.1:8000/restaurants/get/"+ restaurantName +"/", {
+        // check if name already exists from database
+        fetch("http://127.0.0.1:8000/restaurants/get/" + restaurantName + "/", {
             method: "GET",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
+            },
+        }).then((response) => {
+            if (response.ok) {
+                setName("");
+                setNameNotification(false);
+            } else if (response.status == 404) {
+                setName(restaurantName);
+                setNameNotification(true);
+            } else {
+                alert("Error: " + response.status);
+                setName("");
+                setNameNotification(false);
             }
-        })
-        .then(response => {
-            if (response.ok){
-                return false
-            }
-            else if (response.status == 404){
-                return true
-            }
-            else{
-                alert("Error: " + response.status)
-                return false
-            }
-        })
-        
-       
-    }
+        });
+    };
+
+    const validate_address = (address) => {
+        if (address.length > 0) {
+            setAddress(address);
+            setAddressNotification(true);
+        } else {
+            setAddress("");
+            setAddressNotification(false);
+        }
+    };
+
+    const validate_postal_code = (postalCode) => {
+        if (postalCode.length > 0) {
+            setPostalCode(postalCode);
+            setPostalCodeNotification(true);
+        } else {
+            setPostalCode("");
+            setPostalCodeNotification(false);
+        }
+    };
+
     return (
         // <Paper style={styles.paperContainer}>
 
@@ -53,34 +112,48 @@ const Details = ({ nextStep, handleChange, state }) => {
                     >
                         <Grid>
                             <TextField
+                                error={!nameNotification}
                                 placeholder="Your restaurant name"
                                 label="Your restaurant name"
-                                onChange={handleChange("name")}
-                                defaultValue={state.name}
-                                // variant="outlined"
+                                onBlur={(e) => validate_name(e.target.value)}
                                 autoComplete="email"
+                                helperText={
+                                    !nameNotification
+                                        ? "A restaurant with this name already exists"
+                                        : ""
+                                }
                                 fullWidth
                             />
 
                             <TextField
+                                error={!addressNotification}
                                 placeholder="Your restaurant's address"
                                 label="Your restaurant's address"
-                                onChange={handleChange("address")}
-                                defaultValue={state.address}
-                                // variant="outlined"
+                                onBlur={(e) => validate_address(e.target.value)}
                                 autoComplete="address"
                                 fullWidth
+                                helperText={
+                                    !addressNotification
+                                        ? "Address cannot be empty"
+                                        : ""
+                                }
                                 style={{ marginTop: "4em" }}
                             />
 
                             <TextField
+                                error={!postalCodeNotification}
                                 placeholder="Postal code"
                                 label="Postal code"
-                                onChange={handleChange("postalCode")}
-                                defaultValue={state.postalCode}
-                                // variant="outlined"
+                                onBlur={(e) =>
+                                    validate_postal_code(e.target.value)
+                                }
                                 autoComplete="postalCode"
                                 fullWidth
+                                helperText={
+                                    !postalCodeNotification
+                                        ? "Postal code cannot be empty"
+                                        : ""
+                                }
                                 style={{ marginTop: "4em" }}
                             />
                             <Button
