199a343eb239a3a809f796a996a01ca4ae9084e1
William Gao <gaoyi.william@gmail.com>
2022-04-20 15:52:29 -0400

    update

--- P2/restaurants/serializers/updateRestaurant.py
+++ P2/restaurants/serializers/updateRestaurant.py
@@ -6,4 +6,4 @@ from restaurants.models import Restaurant
 class UpdateRestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ['restaurant_name', 'address', 'description', 'postal_code', 'phone_number']
+        fields = ['restaurant_name', 'address', 'description', 'postal_code', 'phone_number', 'logo']
