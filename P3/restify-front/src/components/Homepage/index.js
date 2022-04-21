8a9e7e510d89213bcc9eba335dc14406a5a73810
Connor315 <gao315320130@gmail.com>
2022-04-21 19:03:28 -0400

    home

--- P3/restify-front/src/components/Homepage/index.js
+++ P3/restify-front/src/components/Homepage/index.js
@@ -16,19 +16,55 @@ import {Link, Outlet} from "react-router-dom";
 import welcome from "../images/welcome.jpeg";
 import {MDBContainer} from "mdb-react-ui-kit";
 import {Container} from "@material-ui/core";
+import Image from "react-bootstrap/Image";
+import LikedBtn from "../LikeBtn";
+import SimpleImageSlider from "react-simple-image-slider";
 
 // https://www.sololearn.com/Discuss/226630/how-to-make-font-bigger-than-h1-tag
 // https://react-bootstrap.netlify.app/components/carousel/#carousels
 // https://react-bootstrap.github.io/components/overlays/
 
+const ImageSlide = ({ blogID }) =>{
+    const [images, setImages] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:8000/socials/get_blog_image/"+blogID+"/", {
+            method:"GET",
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(json =>{
+                json.map(image=>{
+                    var url = image.image
+                    setImages(images=>[...images, {url}])
+                })
+            })
+
+    }, [])
+
+    return <Container className="align-items-center">
+        {images.length>0?<SimpleImageSlider
+            width={500}
+            height={250}
+            images = {images}
+            showNavs = {true}
+            showBullets = {true}
+            navStyle = {2}
+            style={{marginLeft:"125px"}}>
+        </SimpleImageSlider>:<div/>}
+    </Container>
+}
+
 const HomePage = () => {
     const [rest, setRest] = useState([])
-    const [blog, setBlog] = useState([])
+    const [blogs, setBlogs] = useState([])
     const [notification, setNotification] = useState("")
     const [notification1, setNotification1] = useState("")
 
     useEffect(() => {
-        fetch("http://127.0.0.1:8000/restaurants/get_random_blogs/", {
+        fetch("http://127.0.0.1:8000/restaurants/get_random_rests/", {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
@@ -36,22 +72,43 @@ const HomePage = () => {
             }
         })
             .then((response) => {
-                if (response.ok){
-                    response.json().then((data) =>{
+                if (response.ok) {
+                    response.json().then((data) => {
                         setRest(data.results)
-                        if (data.count === 0){
+                        if (data.count === 0) {
                             setNotification("Waiting for more restaurants...")
-                        }
-                        else{
+                        } else {
                             setNotification("")
-                        }})}})},[])
+                        }
+                    })
+                }
+            });
+        fetch("http://127.0.0.1:8000/socials/get_random_blogs/", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Token " + localStorage.getItem("restifyToken"),
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((data) => {
+                        setBlogs(data.results)
+                        if (data.count === 0) {
+                            setNotification1("Waiting for more blogs...")
+                        } else {
+                            setNotification1("")
+                        }
+                    })
+                }
+            });
+    }, [])
 
 
     return <>
         <MDBContainer fluid style={{height: "100%", backgroundColor: "#e9ebed"}}>
             <Container className="justify-content-center" style={{paddingTop: "3%", paddingBottom: "5%", width: "60%"}}>
                 <OverlayTrigger
-                    key='bottom'
                     placement='bottom'
                     overlay={
                         <Tooltip>
@@ -100,26 +157,31 @@ const HomePage = () => {
                 </Carousel>
             </Container>
 
-            <h4> Recommend restaurants for you: </h4>
-
-            <Container className="justify-content-center" style={{paddingBottom: "5%"}}>
+            <Container className="justify-content-center" style={{width: "100%"}}>
+                <h4> Recommend restaurants </h4>
+                <h5> {notification} </h5>
                 <CardGroup>
                     {rest.map(r =>(
-                        <Card key={r.id}>
-                            <Card.Img variant="top" src={r.logo} />
+                        <Card key={r.id} id={r.id}>
+                            <Card.Img variant="top" src={"http://localhost:8000"+r.logo} style={{ width: '50%', marginBottom: "3%", marginTop: "3%", marginRight: "auto", marginLeft: "auto"}}/>
                             <Card.Body>
-                                <Card.Title>Name: {r.restaurant_name} </Card.Title>
+                                <Card.Title> Name: {r.restaurant_name} </Card.Title>
                                 <Card.Text> Description: {r.description} </Card.Text>
                                 <Card.Text> Address: {r.address} {r.postal_code}</Card.Text>
                                 <Card.Text> Phone Number: {r.phone_number} </Card.Text>
                                 <Button href={"../restaurant/" + r.restaurant_name}>Click to see more information!</Button>
                             </Card.Body>
                             <Card.Footer>
-                                <small className="text-muted">Last updated {r.update_at} mins ago</small>
+                                <small className="text-muted">Last updated at {r.update_at}</small>
                             </Card.Footer>
                         </Card>
                     ))}
                 </CardGroup>
+            </Container>
+
+            <h4> Recommend blogs for you: </h4>
+            <h5> {notification1} </h5>
+            <Container className="justify-content-center" style={{paddingBottom: "5%"}}>
             </Container>
         </MDBContainer>
     </>
