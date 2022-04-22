30825ea0bd88f209a4922b754cff5dbf8a86ca7c
Connor315 <gao315320130@gmail.com>
2022-04-22 10:01:24 -0400

    favourite

--- P3/restify-front/src/components/Favourite/index.js
+++ P3/restify-front/src/components/Favourite/index.js
@@ -13,62 +13,79 @@ const FavouritePage = () => {
     const [rest, setRest] = useState([])
     const [notification, setNotification] = useState("")
     const [info, setInfo] = useState({})
+    const [follow, setFollow] = useState(false)
 
-    // useEffect(() => {
-    //     fetch("http://127.0.0.1:8000/accounts/user/" + localStorage.getItem("username") + "/details/", {
-    //         method: "GET",
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             "Authorization": "Token " + localStorage.getItem("restifyToken"),
-    //         }
-    //     })
-    //         .then((response) => {
-    //             if (response.ok) {
-    //                 return response.json()
-    //             }
-    //         })
-    //         .then(json => setRest(json.followed_restaurant));
-    // }, [])
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/accounts/user/" + localStorage.getItem("username") + "/details/", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Token " + localStorage.getItem("restifyToken"),
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+            })
+            .then(json => setRest(json.followed_restaurant));
+    }, [])
+
+    const unfollow = (e, rest_name) =>{
+        e.preventDefault()
+        fetch('http://127.0.0.1:8000/socials/unfollow/' + rest_name, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Token " + localStorage.getItem("restifyToken"),
+            }
+        }).then((response) =>{
+            if (response.ok){
+                setFollow(true)
+            }
+        })
+    }
 
     return <>
-        {/*<MDBContainer fluid style={{height: "100%", backgroundColor: "#e9ebed"}}>*/}
-        {/*    <Container className="justify-content-center" style={{paddingTop: "3%", paddingBottom: "10%", width: "60%"}}>*/}
-        {/*        {rest.map(r => {*/}
-        {/*            fetch("http://127.0.0.1:8000/restaurants/get/" + r, {*/}
-        {/*                method: "GET",*/}
-        {/*                headers: {*/}
-        {/*                    'Content-Type': 'application/json',*/}
-        {/*                    "Authorization": "Token " + localStorage.getItem("restifyToken"),*/}
-        {/*                }*/}
-        {/*            })*/}
-        {/*                .then((response) => {*/}
-        {/*                    if (response.ok) {*/}
-        {/*                        return response.json()*/}
-        {/*                    }*/}
-        {/*                })*/}
-        {/*                .then(json => setRest(json.followed_restaurant));*/}
-        {/*            return <>*/}
-        {/*                <Card key={r} style={{*/}
-        {/*                    width: '100%',*/}
-        {/*                    marginBottom: "3%",*/}
-        {/*                    marginTop: "3%",*/}
-        {/*                    marginRight: "auto",*/}
-        {/*                    marginLeft: "auto"*/}
-        {/*                }}>*/}
-        {/*                    <Card.Img variant="top" src={"http://127.0.0.1:8000" + r.logo}/>*/}
-        {/*                    <Card.Body>*/}
-        {/*                        <Card.Title>Name: {r.restaurant_name} </Card.Title>*/}
-        {/*                        <Card.Text> Description: {r.description} </Card.Text>*/}
-        {/*                        <Card.Text> Address: {r.address} {r.postal_code}</Card.Text>*/}
-        {/*                        <Card.Text> Phone Number: {r.phone_number} </Card.Text>*/}
-        {/*                        <Button href={"../restaurant/" + r.restaurant_name} variant="light">Click to see more*/}
-        {/*                            information!</Button>*/}
-        {/*                    </Card.Body>*/}
-        {/*                </Card>*/}
-        {/*            </>*/}
-        {/*        })}*/}
-        {/*    </Container>*/}
-        {/*</MDBContainer>*/}
+        <MDBContainer fluid style={{height: "100%", backgroundColor: "#e9ebed"}}>
+            <Container className="justify-content-center" style={{paddingTop: "3%", paddingBottom: "10%", width: "60%"}}>
+                {rest.map(r => {
+                    fetch("http://127.0.0.1:8000/restaurants/get/" + r, {
+                        method: "GET",
+                        headers: {
+                            'Content-Type': 'application/json',
+                            "Authorization": "Token " + localStorage.getItem("restifyToken"),
+                        }
+                    })
+                        .then((response) => {
+                            if (response.ok) {
+                                return response.json()
+                            }
+                        })
+                        .then(json => setInfo(json));
+                    return <>
+                        <Card key={info.id} style={{
+                            width: '100%',
+                            marginBottom: "3%",
+                            marginTop: "3%",
+                            marginRight: "auto",
+                            marginLeft: "auto"
+                        }}>
+                            <Card.Img variant="top" src={info.logo}/>
+                            <Card.Body>
+                                <Card.Title>Name: {info.restaurant_name} </Card.Title>
+                                <Card.Text> Description: {info.description} </Card.Text>
+                                <Card.Text> Address: {info.address} {info.postal_code}</Card.Text>
+                                <Card.Text> Phone Number: {info.phone_number} </Card.Text>
+                                <Button href={"../restaurant/" + info.restaurant_name} variant="light">Click to see more
+                                    information!</Button>
+                                <Button variant="danger" disabled={follow} onClick={(e) => unfollow(e, info.restaurant_name)}>Unfollow</Button>
+                            </Card.Body>
+                        </Card>
+                    </>
+                })}
+            </Container>
+        </MDBContainer>
     </>
 }
 
