81ea13d3303fd8d7cfa6305440cf49d886345387
Connor315 <gao315320130@gmail.com>
2022-04-22 15:43:34 -0400

    r

--- P3/restify-front/src/components/Restaurant/RestaurantPage.js
+++ P3/restify-front/src/components/Restaurant/RestaurantPage.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Route, useParams } from "react-router-dom";
-import Menu from "../Menu";
 import Container from 'react-bootstrap/Container'
 import RestaurantSideBar from "./RestaurantSideBar";
 import Header from "./RestaurantMain/Header.jsx";
 import RestaurantLike from "../LikeBtn/restaurantLike";
+import {Card} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import {MDBContainer} from "mdb-react-ui-kit";
+import {useNavigate} from "react-router-dom";
 
 const RestaurantPage = () => {
+    const navigate = useNavigate()
     let { restaurantName } = useParams();
-    const [address, setAddress] = React.useState("");
-    const [postalCode, setPostalCode] = React.useState("");
-    const [description, setDescription] = React.useState("");
-    const [logo, setLogo] = React.useState("");
-    const [numFollower, setNumFollower] = React.useState(0);
-    const [numLike, setNumLike] = React.useState(0);
-    const [liked_users, setLikedUsers] = React.useState([]);
+    const [address, setAddress] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [description, setDescription] = useState("");
+    const [logo, setLogo] = useState("");
+    const [numFollower, setNumFollower] = useState(0);
+    const [numLike, setNumLike] = useState(0);
+    const [liked_users, setLikedUsers] = useState([]);
+    const [numBlogs, setNumBlogs] = useState(0);
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [follow, setFollow] = useState(false)
 
-    // fetch restaurant data
-    React.useEffect(() => {
+    useEffect(() => {
         fetch("http://127.0.0.1:8000/restaurants/get/"+restaurantName+"/", {
             method: "GET",
             headers: {
@@ -35,27 +41,87 @@ const RestaurantPage = () => {
                         setLogo(data.logo);
                         setNumFollower(data.num_follower);
                         setNumLike(data.num_like);
-                        setLikedUsers(data.liked_users);   
+                        setLikedUsers(data.liked_users);
+                        setNumBlogs(data.num_blog);
+                        setPhoneNumber(data.phone_number)
                     });
+
+                    fetch('http://127.0.0.1:8000/socials/follow/' + restaurantName +"/", {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            "Authorization": "Token " + localStorage.getItem("restifyToken"),
+                        }
+                    }).then((response) =>{
+                        if (response.ok){
+                            setFollow(false)
+                        }
+                        else if (response.status === 400){
+                            setFollow(true)
+                        }
+                    })
                 }
                 else {
-                    // return 404 page
-                    alert("404");
+                    navigate("../../../notFound")
                 }
             });
     }, []);
 
-    
+    const followunfollow = (e) =>{
+        e.preventDefault()
+        if (follow){
+            fetch('http://127.0.0.1:8000/socials/follow/' + restaurantName +"/", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Token " + localStorage.getItem("restifyToken"),
+                }
+            }).then((response) =>{
+                if (response.ok){
+                    setFollow(false)
+                }
+            })
+        }
+        else{
+            fetch('http://127.0.0.1:8000/socials/unfollow/' + restaurantName +"/", {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Token " + localStorage.getItem("restifyToken"),
+                }
+            }).then((response) =>{
+                if (response.ok){
+                    setFollow(true)
+                }
+            })
+        }
+    }
+
+
     return <>
         <Container fluid>
             <div className = "row p-0 flex-nowrap" >
                 <div className="col-3" style={{"paddingLeft": 0, "backgroundColor": "#415973"}}>
                     <RestaurantSideBar/>
                 </div>
-                <div className="col-9 md-auto">
-                    <Header restaurantName={restaurantName} pass_logo={logo}/>
-                    <RestaurantLike restaurantName={restaurantName} numLikes={numLike} initState={liked_users.indexOf(localStorage.getItem("username")) > -1?true:false}/>
-                </div>
+
+                <MDBContainer fluid style={{height: "100%", backgroundColor: "#e9ebed"}}>
+                    <div className="col-9 md-auto">
+                        <Header restaurantName={restaurantName} pass_logo={logo}/>
+                        <RestaurantLike restaurantName={restaurantName} numLikes={numLike} initState={liked_users.indexOf(localStorage.getItem("username")) > -1?true:false}/>
+                    </div>
+
+                    <Container className="justify-content-center" style={{paddingTop: "2%", paddingBottom: "10%", width: "60%"}}>
+                        <Card.Body> <h1> Number of Followers: {numFollower}  |  Number of Likes: {numLike} </h1></Card.Body>
+                        <Card.Body> <h2> Address: {address} </h2> </Card.Body>
+                        <Card.Body> <h2> Phone Number: {phoneNumber} </h2> </Card.Body>
+                        <Card.Body> <h2> Postal Code: {postalCode} </h2> </Card.Body>
+                        <Card.Body> <h2> Number of Blogs: {numBlogs} </h2> </Card.Body>
+                        <Card.Body> <h2> Description: {description} </h2> </Card.Body>
+                        <Button variant="danger"
+                                onClick={(e) => followunfollow(e)}>{follow? "Follow" : "Unfollow"}</Button>
+                    </Container>
+                </MDBContainer>
             </div>
         </Container>
     </>
