b771badd8bd83a33551f5539d912fc140009ae36
Connor315 <gao315320130@gmail.com>
2022-04-23 02:57:51 -0400

    blogpage

--- P3/restify-front/src/components/MyBlogPage/index.js
+++ P3/restify-front/src/components/MyBlogPage/index.js
@@ -131,7 +131,7 @@ const MyBlogPage = () => {
 
     return <>
         <MDBContainer fluid style={{height: "100%", backgroundColor: "#e9ebed"}}>
-            <Container className="justify-content-center" style={{paddingTop: "3%", paddingBottom: "10%", width: "60%"}}>
+            <Container className="justify-content-center" style={{paddingTop: "3%", paddingBottom: "10%", width: "100%"}}>
                 <Table />
                 <div ref={loader} />
             </Container>
