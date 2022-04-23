9507a33bd11ab42cb59160f8bfbc3c455f2c3a72
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-04-23 02:30:56 -0400

    s

--- P2/socials/serializers/getBlog.py
+++ P2/socials/serializers/getBlog.py
@@ -7,6 +7,7 @@ class GetBlogSerializer(serializers.ModelSerializer):
     restaurant = serializers.CharField(source='restaurant.get_name',
                                        read_only=True)
     liked_users = serializers.ReadOnlyField()
+    num_likes = serializers.IntegerField()
 
     class Meta:
         model = Blog
